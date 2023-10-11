84f3c77088405ae85cea2fbc796c6c568efa6b52
Steve van Loben Sels <svanlobensels@rippling.com>
2023-10-11 15:31:55 -0700

    Rename Pension kit's UpdatePayrollContributions -> ReportPayrollContributions
    Also: combine the get file name hook with the format contributions hook to
          avoid chatter between rippling and third party code
    
    This name change is to better reflect the direction of information flow as
    we're not actually updating anything within Rippling.

--- flux_sdk/pension/capabilities/report_payroll_contributions/interface.py
+++ flux_sdk/pension/capabilities/report_payroll_contributions/interface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from flux_sdk.flux_core.data_models import File
 from flux_sdk.pension.capabilities.report_payroll_contributions.data_models import (
     EmployeePayrollRecord,
     PayrollUploadSettings,
@@ -17,20 +18,13 @@ class ReportPayrollContributions(ABC):
 
     @staticmethod
     @abstractmethod
-    def get_file_name(payroll_upload_settings: PayrollUploadSettings) -> str:
-        """
-        This method receives a PayrollUploadSettings from which the developer is expected to return file name based on payroll_upload_settings
-        :param payroll_upload_settings:
-        :return: str
-        """
-
-    @staticmethod
-    @abstractmethod
     def format_contributions(employee_payroll_records: list[EmployeePayrollRecord],
-                             payroll_upload_settings: PayrollUploadSettings) -> bytes:
+                             payroll_upload_settings: PayrollUploadSettings) -> File:
         """
-        This method receives a list of EmployeePayrollRecord. The developer is expected to return the bytes of the formatted contributions
+        Given a list of EmployeePayrollRecord and the PayrollUploadSettings, prepare a file for upload to the pension
+        provider.  The file will be sent verbatim, so any compression or other formatting required by the pension
+        provider must be applied within this function.
         :param employee_payroll_records:
         :param payroll_upload_settings:
-        :return: bytes
+        :return: File
         """
