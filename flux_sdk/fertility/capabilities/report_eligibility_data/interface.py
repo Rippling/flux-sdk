aa869758de77c7d344fda39d070272518e83a776
Harry He <hhe@rippling.com>
2023-11-09 10:55:48 -0800

    Resovle review concerns

--- flux_sdk/fertility/capabilities/report_eligibility_data/interface.py
+++ flux_sdk/fertility/capabilities/report_eligibility_data/interface.py
@@ -4,10 +4,10 @@ from flux_sdk.flex.capabilities.update_contributions.data_models import Employee
 from flux_sdk.flux_core.data_models import CompanyInfo, File
 
 
-class UpdateEligibilityData(ABC):
+class ReportEligibilityData(ABC):
     """
     This class represents the "upload eligibility data" capability. The developer is supposed to implement
-    get_formatted_eligibility_data_file in their implementation. For further details regarding their
+    format_eligibility_data_file in their implementation. For further details regarding their
     implementation details, check their documentation.
 
     An instance of this class cannot be initiated unless this method is implemented.
@@ -15,14 +15,14 @@ class UpdateEligibilityData(ABC):
 
     @staticmethod
     @abstractmethod
-    def get_formatted_eligibility_data_file(
+    def format_eligibility_data_file(
         company_info: CompanyInfo,
         employee_eligibility_records: list[EmployeeEligibilityRecord]
     ) -> File:
         """
-        Given a list of EmployeeEligibilityRecord, prepare a file for upload to the provider Carrot.
+        Given a list of EmployeeEligibilityRecord, prepare a file for upload to the provider.
         The file will be sent SFTP, so any compression or other formatting required by the provider
-        Carrot must be applied within this function.
+        must be applied within this function.
         :param company_info:
         :param employee_eligibility_records:
         :return:
