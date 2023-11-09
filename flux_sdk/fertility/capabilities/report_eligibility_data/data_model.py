3a485000087ac0e629591132f63c84047da147ca
Harry He <hhe@rippling.com>
2023-11-09 13:43:50 -0800

    Use Employee class

--- flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
+++ flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
@@ -2,24 +2,12 @@ from datetime import date
 from enum import Enum
 from typing import Optional
 
-
-class Gender(Enum):
-    MALE = "MALE"
-    FEMALE = "FEMALE"
-    NON_BINARY = "NON_BINARY"
-    DECLINE_TO_SELF_IDENTIFY = "DECLINE_TO_SELF_IDENTIFY"
-
+from flux_sdk.flux_core.data_models import Employee
 
 class EmployeeEligibilityRecord:
-    role_id: str
+    employee: Employee
     company_id: str
-    first_name: str
-    last_name: str
-    email: str
     start_date: date
-    dob: date
-    gender: Gender
-    zip: str
     end_date: date
     dependent_first_name: Optional[str]
     dependent_last_name: Optional[str]
