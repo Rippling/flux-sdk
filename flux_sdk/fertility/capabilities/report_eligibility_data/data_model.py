3d2ba5f45e3217565c8d3a746eca134526a93b7a
Harry He <hhe@rippling.com>
2023-11-09 12:10:10 -0800

    Resolve more review concerns

--- flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
+++ flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
@@ -1,8 +1,25 @@
 from datetime import date
-from decimal import Decimal
 from enum import Enum
 from typing import Optional
 
 
+class Gender(Enum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+    NON_BINARY = "NON_BINARY"
+    DECLINE_TO_SELF_IDENTIFY = "DECLINE_TO_SELF_IDENTIFY"
+
+
 class EmployeeEligibilityRecord:
-    pass
+    role_id: str
+    company_id: str
+    first_name: str
+    last_name: str
+    email: str
+    start_date: date
+    dob: date
+    gender: Gender
+    zip: str
+    end_date: date
+    dependent_first_name: Optional[str]
+    dependent_last_name: Optional[str]
--- flux_sdk/fertility/capabilities/report_eligibility_data/interface.py
+++ flux_sdk/fertility/capabilities/report_eligibility_data/interface.py
@@ -7,21 +7,20 @@ from flux_sdk.flux_core.data_models import CompanyInfo, File
 class ReportEligibilityData(ABC):
     """
     This class represents the "report eligibility data" capability. The developer is supposed to implement
-    format_eligibility_data_file in their implementation. For further details regarding their
-    implementation details, check their documentation.
+    format_eligibility_file in their implementation.
 
     An instance of this class cannot be initiated unless this method is implemented.
     """
 
     @staticmethod
     @abstractmethod
-    def format_eligibility_data_file(
+    def format_eligibility_file(
         company_info: CompanyInfo,
         employee_eligibility_records: list[EmployeeEligibilityRecord]
     ) -> File:
         """
         Given a list of EmployeeEligibilityRecord, prepare a file for upload to the provider.
-        The file will be sent SFTP, so any compression or other formatting required by the provider
+        The file will be sent verbatim, so any compression or other formatting required by the provider
         must be applied within this function.
         :param company_info:
         :param employee_eligibility_records:
