9811a160f3372a277a3d49ba1807d88dbcfb8323
Harry He <hhe@rippling.com>
2023-11-09 15:22:35 -0800

    more changes

--- flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
+++ flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
@@ -6,8 +6,7 @@ from flux_sdk.flux_core.data_models import Employee
 
 class EmployeeEligibilityRecord:
     employee: Employee
-    company_id: str
-    start_date: date
-    end_date: date
+    eligibility_start_date: date
+    eligibility_end_date: date
     dependent_first_name: Optional[str]
     dependent_last_name: Optional[str]
