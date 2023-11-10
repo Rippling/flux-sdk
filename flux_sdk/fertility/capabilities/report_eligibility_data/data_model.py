635a927c2319ef75eab35a243395452258056338
Harry He <hhe@rippling.com>
2023-11-09 20:41:18 -0800

    resolve review concerns

--- flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
+++ flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
@@ -6,7 +6,13 @@ from flux_sdk.flux_core.data_models import Employee
 
 class EmployeeEligibilityRecord:
     employee: Employee
-    eligibility_start_date: date
-    eligibility_end_date: date
     dependent_first_name: Optional[str]
     dependent_last_name: Optional[str]
+
+    # eligibility_start_date is the max of the employee start date considering plan waiting period and the plan
+    # effective date
+    eligibility_start_date: Optional[date]
+
+    # If the employee is terminated, eligibility_end_date is the min of end date considering plan termination policy
+    # and the plan expiration date; otherwise, it's the plan expiration date
+    eligibility_end_date: Optional[date]
