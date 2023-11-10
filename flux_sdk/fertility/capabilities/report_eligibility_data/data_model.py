b5ddcbc6570e99e31deb7da053b6e5fb852c1c37
Harry He <zhedahht@hotmail.com>
2023-11-10 10:25:47 -0800

    Create kit fertility (#13)
    Create kit fertility_carrot

---
+++ flux_sdk/fertility/capabilities/report_eligibility_data/data_model.py
@@ -0,0 +1,18 @@
+from datetime import date
+from enum import Enum
+from typing import Optional
+
+from flux_sdk.flux_core.data_models import Employee
+
+class EmployeeEligibilityRecord:
+    employee: Employee
+    dependent_first_name: Optional[str]
+    dependent_last_name: Optional[str]
+
+    # eligibility_start_date is the max of the employee start date considering plan waiting period and the plan
+    # effective date
+    eligibility_start_date: Optional[date]
+
+    # If the employee is terminated, eligibility_end_date is the min of end date considering plan termination policy
+    # and the plan expiration date; otherwise, it's the plan expiration date
+    eligibility_end_date: Optional[date]
---
+++ flux_sdk/fertility/capabilities/report_eligibility_data/interface.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+from flux_sdk.flex.capabilities.update_contributions.data_models import EmployeeEligibilityRecord
+from flux_sdk.flux_core.data_models import CompanyInfo, File
+
+
+class ReportEligibilityData(ABC):
+    """
+    This class represents the "report eligibility data" capability. The developer is supposed to implement
+    format_eligibility_file in their implementation.
+
+    An instance of this class cannot be initiated unless this method is implemented.
+    """
+
+    @staticmethod
+    @abstractmethod
+    def format_eligibility_file(
+        company_info: CompanyInfo,
+        employee_eligibility_records: list[EmployeeEligibilityRecord]
+    ) -> File:
+        """
+        Given a list of EmployeeEligibilityRecord, prepare a file for upload to the provider.
+        The file will be sent verbatim, so any compression or other formatting required by the provider
+        must be applied within this function.
+        :param company_info:
+        :param employee_eligibility_records:
+        :return:
+        """
