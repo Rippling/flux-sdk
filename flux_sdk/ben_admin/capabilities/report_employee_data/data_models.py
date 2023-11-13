2e2343effedfede86d54d15d2ce9a0ec5480b63b
Bryan Bierce <bbierce@rippling.com>
2023-11-13 12:13:42 -0800

    update models. bump version

--- flux_sdk/ben_admin/capabilities/report_employee_data/data_models.py
+++ flux_sdk/ben_admin/capabilities/report_employee_data/data_models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from enum import Enum
 from typing import Optional
 
-from flux_sdk.flux_core.data_models import Address, EmployeeState, Gender, MaritalStatus
+from flux_sdk.flux_core.data_models import Address, EmployeeState, Gender
 
 
 class AppConfig:
@@ -25,7 +25,7 @@ class Name:
         return f"${self.first} ${self.last}"
     
 
-class EmployementType(Enum):
+class EmploymentType(Enum):
     CONTRACTOR = 1
     SALARIED_FT = 2
     SALARIED_PT = 3
@@ -34,17 +34,60 @@ class EmployementType(Enum):
     TEMP = 6
 
 
-class Employment:
-    type: EmployementType
-    is_rehire: bool
-    termination_date: Optional[datetime]
-    start_date: datetime
-    start_date: datetime
-    original_hire_date: datetime
+class MonetaryValue:
+    value: float
+    currency_type: str
+
+
+class PayFrequency(Enum):
+    WEEKLY = 1
+    BI_WEEKLY = 2
+    MONTHLY = 3
+    SEMI_MONTHLY = 4
+
+class PayTimeUnit(Enum):
+    HOUR = 1
+    DAY = 2
+    WEEK = 3
+    MONTH = 4
+    YEAR = 5 
+    PAY_PERIOD = 6
 
 
 class Pay:
+    frequency: PayFrequency
+    frequency_effective_date: datetime
+    time_unit: PayTimeUnit
+    value_per_unit: MonetaryValue
+    value_effective_date: datetime
+
+class EmploymentHours:
+    type: EmploymentType
+    effectiveDate: datetime
+    hours_per_week: Optional[int]
+
+class Employment:
+    hours: EmploymentHours
+    is_rehire: bool
+    termination_date: Optional[datetime]
+    start_date: datetime
+    original_hire_date: datetime
     w2_start_date: datetime
+    pay: Pay
+    
+
+class BenefitsEligibility(Enum):
+    # this will expand to cover cobra later
+    ELIGIBLE = 1
+    IN_ELIGIBLE = 2
+
+class BenefitsEligibilityStatus:
+    eligibility: BenefitsEligibility
+    effective_date: datetime
+
+class EmployeeStatus:
+    status: EmployeeState
+    effective_date: datetime
 
 class EmployeeData:
     '''
@@ -53,15 +96,14 @@ class EmployeeData:
     id: str
     ssn: str
     name: Name
-    insurance_eligible: bool
+    dob : datetime
+    phone_number: str
     business_email: str
     personal_email: str
     gender: Gender
     employment: Employment
     pay: Pay
     address: Address
-    status: EmployeeState
-    dob : datetime
-    phone_number: str
-    marital_status: Optional[MaritalStatus]
+    status: EmployeeStatus
+    benefits: BenefitsEligibilityStatus
     
