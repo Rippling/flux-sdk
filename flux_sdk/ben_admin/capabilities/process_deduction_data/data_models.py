802ac7f1b9a24618b379129d5984cabc84602e33
Bryan Bierce <bbierce@rippling.com>
2023-11-13 12:23:28 -0800

    add deductions app config;

---
+++ flux_sdk/ben_admin/capabilities/process_deduction_data/data_models.py
@@ -0,0 +1,5 @@
+class AppConfig:
+    '''
+    This contains the application data gathered durring installation which is necesary to process employee data and send to payroll
+    '''
+    group_id: str
