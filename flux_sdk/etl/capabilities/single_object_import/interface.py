f36e1c361185b7543e89c31463524de98c01bd16
Dominic Barnes <dbarnes@rippling.com>
2023-12-07 15:13:55 -0800

    add more comments

--- flux_sdk/etl/capabilities/single_object_import/interface.py
+++ flux_sdk/etl/capabilities/single_object_import/interface.py
@@ -19,6 +19,12 @@ class SingleObjectImport(ABC):
         """
         Use this hook to indicate the structure (aka: schema) for the data being imported. This is used to ensure that
         Rippling Custom Objects has a target for the extracted records to be loaded into.
+
+        In Custom Objects, fields are not able to change their data type once they are created. As a result, new fields
+        will be detected and created as expected. For existing fields, a change to a data type will result in an error,
+        while other metadata changes (eg: description, enum_values) will be applied. For the time-being, migration must
+        be performed by adding new fields.
+
         :return: Schema
         """
 
@@ -27,6 +33,9 @@ class SingleObjectImport(ABC):
     def prepare_query(schema: Schema, checkpoint: Optional[Checkpoint]) -> Query:
         """
         Use this hook to construct the query that should be run by Rippling to extract records.
+
+        The "process_records" hook must decorate records with checkpoint in order to enable incremental sync.
+
         :param schema: The schema generated in the "get_schema" hook for this object.
         :param checkpoint: If included, this is an incremental sync and should adjust the Query accordingly.
         :return: Query
@@ -37,8 +46,13 @@ class SingleObjectImport(ABC):
     def process_records(schema: Schema, records: list[Record]) -> list[Record]:
         """
         Use this hook to post-process the Records extracted via the query performed from "prepare_query". This hook is
-        required in order to implement incremental sync (via Record.checkpoint) or to dynamically drop records from the
-        sync (via Record.drop).
+        required in order to implement incremental sync (via checkpoint) or to dynamically drop records from the sync
+        (via drop).
+
+        If the Record does not adhere to the provided schema, such as using an incorrect/incompatible type for a field,
+        the sync will fail. As such, this hook is the opportunity to massage any raw query results that may not be
+        directly compatible.
+
         :param schema: The schema generated in the "get_schema" hook for this object.
         :param records: This batch of records should be updated
         :return: Query
