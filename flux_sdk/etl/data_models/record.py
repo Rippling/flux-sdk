3501d42a26a27773a2a6ed8b9d67cadf8164107f
Dominic Barnes <dbarnes@rippling.com>
2023-12-11 13:48:23 -0800

    make Record.primary_key a str only (conversions happen internally)

--- flux_sdk/etl/data_models/record.py
+++ flux_sdk/etl/data_models/record.py
@@ -2,10 +2,6 @@ from dataclasses import dataclass
 from datetime import date, datetime, time
 from typing import Optional, Union
 
-# This indicates what types can be used for a primary key. Using a string is recommended, as numeric values will be
-# coerced into strings within Rippling.
-PrimaryKey = Union[str, int]
-
 # This represents the currently supported types for Record fields. Currently, we only support these primitive types and
 # do not allow for any complex/nested types.
 Field = Union[str, int, float, bool, date, time, datetime, None]
@@ -21,7 +17,7 @@ Checkpoint = Union[datetime, int, str]
 class Record:
     # The ID for this record, which becomes external_id in the Custom Object. Since only a simple, string value is
     # supported by Custom Objects, this does not need to be a full SchemaField.
-    primary_key: PrimaryKey
+    primary_key: str
 
     # This represents the links in this object. Each key is the field name, which is used to look up the target details.
     # Each value is the identifier used for the link.
@@ -30,10 +26,6 @@ class Record:
     # This is the rest of the raw data for this record.
     fields: dict[str, Field]
 
-    # This flag can be used by the "process_records" hook to signal to Rippling that the object should not be imported.
-    # If a Record is not found after this hook, that will be regarded as an error, so this flag should be used instead.
-    drop: bool
-
     # This is used by the "process_records" hook to signal to Rippling the checkpoint that can be observed and recorded
     # after the sync is complete. Rippling will keep track of the *highest* value seen from all records. That value will
     # be passed to the "prepare_query" hook and can be used to adjust the query to only retrieve the records that have
@@ -41,3 +33,7 @@ class Record:
     #
     # This must be used to enable incremental sync, otherwise a full sync will happen each time.
     checkpoint: Optional[Checkpoint]
+
+    # This flag can be used by the "process_records" hook to signal to Rippling that the object should not be imported.
+    # If a Record is not found after this hook, that will be regarded as an error, so this flag should be used instead.
+    drop: bool
