9e4323a5d9a669ece16f1efb337be8fa163db648
Shanmukha Rao <shrao@rippling.com>
2024-04-05 07:30:44 +0000

    Also pass ruff config as parameter

--- run_ruff.sh
+++ run_ruff.sh
@@ -21,9 +21,9 @@ ensure_ruff_installed() {
 run_ruff() {
     ruff_cmd="ruff check"
     if [[ "$2" != "" ]]; then
-        ruff_cmd="$ruff_cmd $2 $1"
+        ruff_cmd="$ruff_cmd $2 $1  --config ruff.toml"
     else
-        ruff_cmd="$ruff_cmd $1"
+        ruff_cmd="$ruff_cmd $1  --config ruff.toml"
     fi
     # shellcheck disable=SC2086
     "${PYTHON_EXECUTABLE}" -m $ruff_cmd
