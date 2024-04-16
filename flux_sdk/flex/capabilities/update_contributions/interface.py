f4106915c45fb9d4c9247a12cec5a122de43b1a6
Shanmukha Rao <shrao@rippling.com>
2024-04-16 09:11:21 +0530

    Documentation changes

--- flux_sdk/flex/capabilities/update_contributions/interface.py
+++ flux_sdk/flex/capabilities/update_contributions/interface.py
@@ -5,7 +5,9 @@ from flux_sdk.flux_core.data_models import AppContext, CompanyInfo, File
 
 
 class UpdateContributions(ABC):
-    """
+    """Report employee payroll contributions for contribution types (FSA, HSA, etc.) in a payroll run to a benefit
+     provider.
+
     This class represents the "update contributions" capability. The developer is supposed to implement
     get_formatted_enrollment_files in their implementation. For further details regarding their
     implementation details, check their documentation.
@@ -17,10 +19,12 @@ class UpdateContributions(ABC):
     @abstractmethod
     def get_formatted_contributions_files(company_info: CompanyInfo, employee_contributions: list[EmployeeContribution],
                                           app_context: AppContext) -> list[File]:
-        """
+        """A function that converts employee contributions to formatted contribution file.
+
         This method receives company information and a list of EmployeeContribution objects. The developer is expected
         to return the app specific list of formatted contribution files.
         :param company_info:
         :param employee_contributions:
+        :param app_context:
         :return:
         """
