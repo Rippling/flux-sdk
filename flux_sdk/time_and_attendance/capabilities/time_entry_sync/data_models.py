5b47057b3f46420cd1417f05c9084141981cdad1
Sihan Wang <siwang@rippling.com>
2024-09-25 16:28:57 -0700

    add doc strings'

--- flux_sdk/time_and_attendance/capabilities/time_entry_sync/data_models.py
+++ flux_sdk/time_and_attendance/capabilities/time_entry_sync/data_models.py
@@ -8,10 +8,25 @@ class TimeEntriesQuery:
     all of these filters, it is us to app developer to determine how to handle the case
     where unsupported filters are passed in.
     """
+    """
+    time_entry_ids: This field denotes list of 3rd party time entry ids
+    """
     time_entry_ids: Optional[list[str]]
+    """
+    organization_id: This field denotes the 3rd party organization id
+    """
     organization_id: Optional[str]
+    """
+    start_time: This field denotes the time entry start datetime (after) to filter for
+    """
     start_time: Optional[datetime]
+    """
+    modified_time: This field denotes the time entry modified datetime (after) to filter for
+    """
     modified_time: Optional[datetime]
+    """
+    end_time: This field denotes the time entry end datetime (before) to filter for
+    """
     end_time: Optional[datetime]
 
 class JobShift:
@@ -20,10 +35,25 @@ class JobShift:
     names in different apps, but should reflect clocked in periods. Note that these may overlap
     in time with breaks but must not overlap with other JobShifts.
     """
+    """
+    id: This field denotes the 3rd party job shift id
+    """
     id: Optional[str]
+    """
+    job_title: This field denotes the 3rd party job title or job id
+    """
     job_title: str
+    """
+    start_time: This field denotes the job shift start datetime
+    """
     start_time: Optional[datetime]
+    """
+    end_time: This field denotes the job shift end datetime
+    """
     end_time: Optional[datetime]
+    """
+    description: This field denotes the 3rd party job shift description
+    """
     description: Optional[str]
 
 class Break:
@@ -32,9 +62,21 @@ class Break:
     names in different apps, but should reflect break periods. Note that these may overlap
     in time with JobShifts but must not overlap with other Breaks.
     """
+    """
+    id: This field denotes the 3rd party break id
+    """
     id: Optional[str]
+    """
+    start_time: This field denotes the break start datetime
+    """
     start_time: Optional[datetime]
+    """
+    end_time: This field denotes the break end datetime
+    """
     end_time: Optional[datetime]
+    """
+    description: This field denotes the 3rd party break description
+    """
     description: Optional[str]
 
 class TimeEntry:
@@ -43,8 +85,23 @@ class TimeEntry:
     names in different apps, but should reflect the overarching record that tracks time segments of
     work and breaks as child lists.
     """
+    """
+    id: This field denotes the 3rd party time entry id
+    """
     id: str
+    """
+    user_id: This field denotes the 3rd party user id this time entry belongs to
+    """
     user_id: str
+    """
+    organization_id: This field denotes the 3rd party organization id this time entry belongs to
+    """
     organization_id: Optional[str]
+    """
+    job_shifts: This field denotes the list of 3rd party job shifts this time entry contains
+    """
     job_shifts: Optional[list[JobShift]]
+    """
+    breaks: This field denotes the list of 3rd party breaks this time entry contains
+    """
     breaks: Optional[list[Break]]
