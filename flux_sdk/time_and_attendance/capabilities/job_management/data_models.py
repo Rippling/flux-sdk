8e569a3fe1cb5efd6ec0b245e162d996aa56cc98
Dharmik Shah <dhshah@rippling.com>
2024-09-25 17:45:58 -0400

    suggestions

--- flux_sdk/time_and_attendance/capabilities/job_management/data_models.py
+++ flux_sdk/time_and_attendance/capabilities/job_management/data_models.py
@@ -10,11 +10,11 @@ class RipplingAttribute(Enum):
     This represents the supported attributes in Rippling.
     Your third-party system attributes should be mapped to one or more of these attributes.
     """
-    Department = "DEPARTMENT"
-    WorkLocation = "WORK_LOCATION"
-    PayRate = "PAY_RATE"
-    Ein = "EIN"
-    JobSiteLocation = "JOB_SITE_LOCATION"
+    DEPARTMENT = "DEPARTMENT"
+    WORK_LOCATION = "WORK_LOCATION"
+    PAY_RATE = "PAY_RATE"
+    EIN = "EIN"
+    JOB_SITE_LOCATION = "JOB_SITE_LOCATION"
 
 @dataclass(kw_only=True)
 class Attribute:
@@ -31,7 +31,7 @@ class Attribute:
     description: Optional[str] = ""
     """The optional description associated with the attribute."""
 
-    matching_rippling_attribute: list[RipplingAttribute]
+    compatible_rippling_attribute: list[RipplingAttribute]
     """The list of Rippling attributes that match the attribute."""
 
     def __post_init__(self):
@@ -39,10 +39,10 @@ class Attribute:
         check_field(self, "id", str, required=True)
         check_field(self, "name", str, required=True)
         check_field(self, "description", str)
-        check_field(self, "matching_rippling_attribute", list[RipplingAttribute], required=True)
+        check_field(self, "compatible_rippling_attribute", list[RipplingAttribute], required=True)
 
-        if len(self.matching_rippling_attribute) == 0:
-            raise ValueError("matching_rippling_attribute must have at least 1 value")
+        if len(self.compatible_rippling_attribute) == 0:
+            raise ValueError("compatible_rippling_attribute must have at least 1 value")
 
 @dataclass(kw_only=True)
 class GetJobAttributesResponse:
