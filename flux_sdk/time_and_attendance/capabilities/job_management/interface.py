ec4172e48c3f06475b880ada1bf1f2cd3e812078
dhshah-rippling <dhshah@rippling.com>
2024-10-02 19:53:24 -0400

    [APPS-29594] add get job attributes sync sdk (#88)
    * add sdk
    
    * remove certain fields, adjust comments
    
    * address comments
    
    * fix decimal issue
    
    * add compaitable
    
    * ruff
    
    * remove a
    
    * add attributes request
    
    * rename to requested attributes
    
    * add helper fn
    
    * add compiatbie values global to make sure values dont update across attributes
    
    * names cannot repeat
    
    * change sdk design
    
    * helper func
    
    * helper
    
    * fix requested attributes value error
    
    * remove files
    
    * update version
    
    * list
    
    * add attribute value check

--- flux_sdk/time_and_attendance/capabilities/job_management/interface.py
+++ flux_sdk/time_and_attendance/capabilities/job_management/interface.py
@@ -1,17 +1,28 @@
 from abc import ABC, abstractmethod
 
-from flux_sdk.time_and_attendance.capabilities.job_management.data_models import GetJobAttributesResponse
+from flux_sdk.time_and_attendance.capabilities.job_management.data_models import (
+    GetJobAttributesRequest,
+    GetJobAttributesResponse,
+)
 
 
 class JobManagement(ABC):
-
     @abstractmethod
-    def get_job_attributes(self) -> GetJobAttributesResponse:
+    def get_job_attributes(self, request: GetJobAttributesRequest) -> GetJobAttributesResponse:
         """
         A function that gets job attributes from the third-party system.
 
         Use this hook to fetch job attributes from the third-party system
         so that they can be matched with attributes in Rippling.
-        :return: The response containing the job attributes from the third-party system.
-        """
 
+        If request.requested_attribute_values is False, only fetch the mapping of the attributes to Rippling.
+
+        If request.requested_attribute_values is True, fetch the mapping and the attribute values.
+
+        Only fetch data for the attributes in request.requested_attributes. If requested_attributes is None, fetch all.
+        Note, it is recommended to use the key_in_requested_attributes helper in the requested.
+
+        :param request: The request containing the attributes to fetch and if values are required.
+        :return: The response containing the job attributes from the third-party system, mapping to their Rippling
+        attributes and possible values.
+        """
