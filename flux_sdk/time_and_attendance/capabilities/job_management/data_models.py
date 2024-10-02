7726ecf0fac35337769e570afa76e6d8aae74594
Dharmik Shah <dhshah@rippling.com>
2024-10-02 11:35:41 -0400

    change sdk design

--- flux_sdk/time_and_attendance/capabilities/job_management/data_models.py
+++ flux_sdk/time_and_attendance/capabilities/job_management/data_models.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from decimal import Decimal
 from enum import Enum
-from typing import Dict, List, Optional, Union
+from typing import List, Optional, Union
 
 from flux_sdk.flux_core.validation import check_field
 
@@ -11,49 +11,47 @@ class RipplingAttribute(Enum):
     This represents the supported attributes in Rippling.
     Your third-party system attributes should be mapped to one or more of these attributes.
     """
+
     DEPARTMENT = "DEPARTMENT"
     WORK_LOCATION = "WORK_LOCATION"
     PAY_RATE = "PAY_RATE"
     EIN = "EIN"
     JOB_SITE_LOCATION = "JOB_SITE_LOCATION"
 
+
 @dataclass(kw_only=True)
-class Attribute:
+class PayRateCompatibleValue:
     """
-    This represents an attribute from the third party system and how it is mapped to Rippling.
+    This represents a pay rate compatible value.
     """
 
-    id: str
-    """The unique id associated with the attribute."""
-
-    name: str
-    """The unique name associated with the attribute."""
-
-    description: Optional[str] = ""
-    """The optional description associated with the attribute."""
-
-    compatible_rippling_attribute: list[RipplingAttribute]
-    """The list of Rippling attributes that are compatible with the attribute, sorted from most compatible to least."""
+    pay_rate: Optional[str] = None
+    """The pay rate associated with the job title, str with max 4 decimals, for ex, 43.3943"""
 
     def __post_init__(self):
         """Perform validation."""
-        check_field(self, "id", str, required=True)
-        check_field(self, "name", str, required=True)
-        check_field(self, "description", str)
-        check_field(self, "compatible_rippling_attribute", list[RipplingAttribute], required=True)
+        if self.pay_rate is not None:
+            if self.pay_rate and not isinstance(self.pay_rate, str):
+                raise ValueError(f"pay_rate must be a str if provided, got {type(self.pay_rate).__name__} instead.")
 
-        if len(self.compatible_rippling_attribute) == 0:
-            raise ValueError("compatible_rippling_attribute must have at least 1 value")
+            try:
+                decimal = Decimal(self.pay_rate)
+            except Exception:
+                raise ValueError("pay_rate must be a valid number")
 
+            if decimal < 0:
+                raise ValueError("pay_rate must be a positive number")
+            if decimal.as_tuple().exponent < -4:
+                raise ValueError("pay_rate must have at most 4 decimal places")
 
 @dataclass(kw_only=True)
-class Address:
+class AddressCompatibleValue:
     """
-    This represents an address.
+    This represents an address compatible value.
     """
 
     street_line_1: str
-    """The first line of the street address."""
+    """The first line of the street address. """
 
     street_line_2: Optional[str] = ""
     """The second line of the street address."""
@@ -82,184 +80,142 @@ class Address:
         if len(self.country_code) != 2 or self.country_code != self.country_code.upper():
             raise ValueError("country_code must be a 2 letter uppercase country code")
 
+
 @dataclass(kw_only=True)
-class PayRateCompatibleValue:
+class AttributeValue:
     """
-    This represents a pay rate compatible value. The name must be unique across all pay rate compatible values.
+    This represents an attribute value associated with an attribute.
+
+    Each attribute value will have a name, and a list of associated attribute values based on the
+    compatible_rippling_attributes field.
     """
 
     name: str
-    """The unique value associated with the attribute, ex: if your attribute was job_title, name would be Cashier."""
+    """
+    The unique value associated with the attribute. Please strip any whitespaces.
+    For ex, if the attribute was "job_title", the value could be "Cashier".
+    """
 
-    pay_rate: Optional[str] = None
-    """The pay rate associated with the job title, str with max 4 decimals, for ex, 43.3943"""
+    associated_attribute_values: List[Union[PayRateCompatibleValue, AddressCompatibleValue]]
+    """
+    The non-empty list of associated attribute values based on compatible_rippling_attributes.
+    For ex, if the attribute was "job_title" and compatible_rippling_attributes was [PAY_RATE], 
+    the associated_attribute_values should be [PayRateCompatibleValue(...)].
+    
+    If the compatible_rippling_attributes was [WORK_LOCATION] or [JOB_SITE_LOCATION] (or both), 
+    the associated_attribute_values could be [AddressCompatibleValue(...)].
+    """
 
     def __post_init__(self):
         """Perform validation."""
         check_field(self, "name", str, required=True)
+        check_field(self, "associated_attribute_values", list, required=True)
 
-        if self.pay_rate is not None:
-            if self.pay_rate and not isinstance(self.pay_rate, str):
-                raise ValueError(f"pay_rate must be a str if provided, got {type(self.pay_rate).__name__} instead.")
+        if len(self.associated_attribute_values) == 0:
+            raise ValueError("associated_attribute_values must have at least 1 value")
 
-            try:
-                decimal = Decimal(self.pay_rate)
-            except Exception:
-                raise ValueError("pay_rate must be a valid number")
-
-            if decimal < 0:
-                raise ValueError("pay_rate must be a positive number")
-            if decimal.as_tuple().exponent < -4:
-                raise ValueError("pay_rate must have at most 4 decimal places")
+        for value in self.associated_attribute_values:
+            if not isinstance(value, (PayRateCompatibleValue, AddressCompatibleValue)):
+                raise ValueError(
+                    "associated_attribute_values must be of type PayRateCompatibleValue or AddressCompatibleValue"
+                )
 
 
 @dataclass(kw_only=True)
-class JobSiteLocationCompatibleValue:
+class Attribute:
     """
-    This represents a job site location compatible value.
-    The name must be unique across all job site location compatible values.
+    This represents an attribute from the third party system and how it is mapped to Rippling.
+
+    Attribute values should also be provided if requested by Rippling.
     """
+
+    id: str
+    """The unique id associated with the attribute, for ex, job_title."""
 
     name: str
-    """The unique name of the job site location, ex: San Francisco."""
+    """The unique name associated with the attribute, for ex, "Job title". Please strip any whitespaces."""
 
-    address: Address
-    """The address of the job site location."""
+    description: Optional[str] = ""
+    """The optional description associated with the attribute."""
+
+    compatible_rippling_attributes: list[RipplingAttribute]
+    """
+    The non-empty list of Rippling attributes that are compatible with the attribute, 
+    sorted from most compatible to least, for ex, [PAY_RATE].
+    """
+
+    attribute_values: Optional[List[AttributeValue]] = None
+    """
+    The non-empty (if not None) list of attribute values associated with the attribute.
+    This field should only be used if requested_attribute_values is True in GetJobAttributesRequest.
+    Note that attribute name should be unique across attribute_values.
+    """
 
     def __post_init__(self):
         """Perform validation."""
+        check_field(self, "id", str, required=True)
         check_field(self, "name", str, required=True)
-        check_field(self, "address", Address, required=True)
+        check_field(self, "description", str)
+        check_field(self, "compatible_rippling_attributes", list, required=True)
 
-@dataclass(kw_only=True)
-class WorkLocationCompatibleValue:
-    """
-    This represents a work location compatible value.
-    The name must be unique across all work location compatible values.
-    """
+        if len(self.compatible_rippling_attributes) == 0:
+            raise ValueError("compatible_rippling_attributes must have at least 1 value")
 
-    name: str
-    """The unique name of the work location, ex: San Francisco."""
+        for attribute in self.compatible_rippling_attributes:
+            if not isinstance(attribute, RipplingAttribute):
+                raise ValueError("compatible_rippling_attributes must be of type RipplingAttribute")
 
-    address: Address
-    """The address of the work location."""
+        if self.attribute_values is not None:
+            if len(self.attribute_values) == 0:
+                raise ValueError("attribute_values must have at least 1 value")
 
-    def __post_init__(self):
-        """Perform validation."""
-        check_field(self, "name", str, required=True)
-        check_field(self, "address", Address, required=True)
-
-@dataclass(kw_only=True)
-class GetJobAttributeValuesResponse:
-    """
-    This represents a response containing the attributes from the third party mapped to their most compatible values.
-
-    The attributes should match the keys returned in the get_job_attributes hook, which is also available in the
-    GetJobAttributeValuesRequest.
-
-    For instance, if the third party system has an attribute "location" that is most compatible with Rippling's
-     "WORK_LOCATION" attribute, and something like the following is defined:
-
-    result = {
-        "location": [WorkLocationCompatibleValue(name="sf",...), WorkLocationCompatibleValue(...)],
-        "job_title": [PayRateCompatibleValue(...)],
-    }
-
-    Do not mix different types of values for the same key. For example, the following is invalid:
-    result = {
-        "job_title": [PayRateCompatibleValue(...), WorkLocationCompatibleValue(...)],
-    }
-
-    Do not repeat *CompatibleValue types across keys. It should be a 1-1 mapping. For example, the following is invalid:
-    result = {
-        "location": [WorkLocationCompatibleValue(name="sf",...), WorkLocationCompatibleValue(...)],
-        "job_title": [WorkLocationCompatibleValue(...)],
-    }
-    """
-
-    result: Dict[str, List[Union[WorkLocationCompatibleValue, PayRateCompatibleValue, JobSiteLocationCompatibleValue]]]
-    """Mapping of attribute name to the list of values that fall under this attribute."""
-
-    def __post_init__(self):
-        """Perform validation."""
-        check_field(self, "result", dict, required=True)
-
-        if len(self.result) == 0:
-            raise ValueError("result must have at least 1 value")
-
-        compatible_values_global = set()
-        for key, value in self.result.items():
-            if not isinstance(key, str):
-                raise ValueError("key must be a string")
-
-            if not isinstance(value, list):
-                raise ValueError("value must be a list")
-
-            if len(value) == 0:
-                raise ValueError("value must have at least 1 value")
-
-            # make sure values are only of one type per key
-            compatible_values = set()
-            compatible_values_name = set()
-            for val in value:
-                if not isinstance(val, (WorkLocationCompatibleValue, PayRateCompatibleValue,
-                                        JobSiteLocationCompatibleValue)):
-                    raise ValueError("value must be of type .*CompatibleValue")
-
-                if type(val) in compatible_values_global:
-                    raise ValueError("value type must be unique across all keys, ie, an attribute")
-
-                if val.name in compatible_values_name:
-                    raise ValueError(f"value name must be unique per key, {val.name} is repeated")
-
-                compatible_values.add(type(val))
-                compatible_values_name.add(val.name)
-            compatible_values_global.update(compatible_values)
-
-            if len(compatible_values) != 1:
-                raise ValueError("value must be of the same type per key")
-
+            names = set()
+            for value in self.attribute_values:
+                if not isinstance(value, AttributeValue):
+                    raise ValueError("attribute_values must be of type AttributeValue")
+                if value.name.strip().lower() in names:
+                    raise ValueError(f"Duplicate name found in attribute_values: {value.name}")
+                names.add(value.name.strip().lower())
 
 
 @dataclass(kw_only=True)
-class GetJobAttributeValuesRequest:
+class GetJobAttributesRequest:
     """
-    This represents a request to fetch the attribute values from the third party system.
+    This represents a request to fetch the attributes from the third party system, how they map to Rippling,
+    and optionally their values.
+
+    If we require the values of the attributes, requested_attribute_values will be True.
+
+    We may also only request information about certain attributes through requested_attributes.
+    """
+
+    requested_attribute_values: bool
+    """
+    Whether to fetch the values of the attributes or not.
+    If False, GetJobAttributesResponse will only contain the list of attributes and how they map to Rippling.
+    If True, GetJobAttributesResponse will also contain the values of the attributes in addition to their mappings.
     """
 
     requested_attributes: List[str] | None = None
     """
-    The attributes to fetch the values for.
-    Use key_in_requested_attributes to determine whether to fetch this attribute or not.
+    The third party attributes to fetch the values for. If None, all attributes should be fetched.
     """
 
-    def __post_init__(self):
-        """Perform validation."""
-
-        check_field(self, "requested_attributes", list, required=False)
-
-        if self.requested_attributes is not None:
-            if len(self.requested_attributes) == 0:
-                raise ValueError("attributes must have at least 1 value")
-            for attribute in self.requested_attributes:
-                if not isinstance(attribute, str):
-                    raise ValueError("attribute must be a string")
-
-    def key_in_requested_attributes(self, key: str) -> bool:
-        """
-        Check if the key is in the requested attributes.
-        Returns True if key exists, or if requested_attributes is None.
-        """
-        return self.requested_attributes is None or key in self.requested_attributes
 
 @dataclass(kw_only=True)
 class GetJobAttributesResponse:
     """
-    This represents a response containing the attributes from the third party system.
+    This represents a response containing the attributes from the third party system, how they map to Rippling,
+    and optionally their values.
+
+    See GetJobAttributesRequest to determine if values are requested, and which attributes are requested.
     """
 
     attributes: list[Attribute]
-    """The attributes from the third party system and how they map to Rippling."""
+    """
+    The non-empty list of attributes from the third party system and how they map to Rippling.
+    The value of the attributes may also be requested.
+    """
 
     def __post_init__(self):
         """Perform validation."""
@@ -273,8 +229,7 @@ class GetJobAttributesResponse:
         for attribute in self.attributes:
             if attribute.id in ids:
                 raise ValueError(f"Duplicate id found: {attribute.id}")
-            if attribute.name in names:
+            if attribute.name.strip().lower() in names:
                 raise ValueError(f"Duplicate name found: {attribute.name}")
             ids.add(attribute.id)
-            names.add(attribute.name)
-
+            names.add(attribute.name.strip().lower())
--- flux_sdk/time_and_attendance/capabilities/job_management/interface.py
+++ flux_sdk/time_and_attendance/capabilities/job_management/interface.py
@@ -1,32 +1,27 @@
 from abc import ABC, abstractmethod
 
 from flux_sdk.time_and_attendance.capabilities.job_management.data_models import (
+    GetJobAttributesRequest,
     GetJobAttributesResponse,
-    GetJobAttributeValuesRequest,
-    GetJobAttributeValuesResponse,
 )
 
 
 class JobManagement(ABC):
-
     @abstractmethod
-    def get_job_attributes(self) -> GetJobAttributesResponse:
+    def get_job_attributes(self, request: GetJobAttributesRequest) -> GetJobAttributesResponse:
         """
         A function that gets job attributes from the third-party system.
 
         Use this hook to fetch job attributes from the third-party system
         so that they can be matched with attributes in Rippling.
-        :return: The response containing the job attributes from the third-party system.
+
+        If request.requested_attribute_values is False, only fetch the mapping of the attributes to Rippling.
+
+        If request.requested_attribute_values is True, fetch the mapping and the attribute values.
+
+        Only fetch data for the attributes in request.requested_attributes. If requested_attributes is None, fetch all.
+
+        :param request: The request containing the attributes to fetch and if values are required.
+        :return: The response containing the job attributes from the third-party system, mapping to their Rippling
+        attributes and possible values.
         """
-
-    @abstractmethod
-    def get_job_attribute_values(self, request: GetJobAttributeValuesRequest) -> GetJobAttributeValuesResponse:
-        """
-        A function that gets job attributes mapped to their values from the third-party system.
-
-        Use this hook to fetch job attributes mapped to their values from the third-party system.
-
-        The values should be formatted to match a Rippling .*CompatibleValue dataclass.
-        :return: The response which is a map of third party attributes to their values.
-        """
-
