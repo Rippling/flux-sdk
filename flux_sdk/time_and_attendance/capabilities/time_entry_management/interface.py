a8cc09a3d1c879c3e519080e76396fabff3c9617
Shimao Zheng <shzheng@rippling.com>
2024-10-17 16:14:52 -0700

    get_break_types hook and data model definitions

--- flux_sdk/time_and_attendance/capabilities/time_entry_management/interface.py
+++ flux_sdk/time_and_attendance/capabilities/time_entry_management/interface.py
@@ -1,15 +1,15 @@
 from abc import ABC, abstractmethod
 
-from flux_sdk.time_and_attendance.capabilities.time_entry_management.data_models import TimeEntriesQuery, TimeEntry
+from flux_sdk.time_and_attendance.capabilities.time_entry_management.data_models import GetBreakTypesResponse, TimeEntriesQuery, TimeEntry
 
 
 class TimeEntryManagement(ABC):
-    """Sync time entry data from 3rd party to Rippling
+    """Sync time entry related data from 3rd party to Rippling
 
-    This class represents the "sync time entry" capability. The developer is supposed to implement
-    fetch_time_entries in their implementation.
+    This class represents the TimeEntryManagement capability. The developer is supposed to implement
+    fetch_time_entries and fetch_break_types in their implementation.
 
-    An instance of this class cannot be initiated unless this method is implemented.
+    An instance of this class cannot be initiated unless both methods are implemented.
     """
 
     @abstractmethod
@@ -19,6 +19,19 @@ class TimeEntryManagement(ABC):
     ) -> list[TimeEntry]:
         """Fetch all time entries for a given query filter.
 
-        :param query:
-        :return:
+        :param query: The query filter to fetch time entries.
+        :return: A list of time entries that match the query.
+        """
+
+    @abstractmethod
+    def get_break_types(self) -> GetBreakTypesResponse:
+        """
+        A function that gets the break types defined in the third party system.
+
+        Use this hook to fetch the break types so that the customer can map them to the break types in Rippling.
+
+        When importing the type entries, the breaks should contain the break type id, which can be resolved to the
+        break type in Rippling.
+
+        :return: The response containing all the break types available in the third party system.
         """
