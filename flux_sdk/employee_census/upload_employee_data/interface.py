ff961ea8b5ea453424eb46776802bf8569b3561c
dhshah-rippling <dhshah@rippling.com>
2024-12-10 11:10:10 -0500

    Update flux_sdk/employee_census/upload_employee_data/interface.py
    Co-authored-by: Shimao Zheng <sz2376@columbia.edu>

--- flux_sdk/employee_census/upload_employee_data/interface.py
+++ flux_sdk/employee_census/upload_employee_data/interface.py
@@ -4,7 +4,7 @@ from flux_sdk.employee_census.upload_employee_data.data_models import EmployeeDa
 from flux_sdk.flux_core.data_models import File
 
 
-class UploadEmployeeData(ABC):
+class UploadEmployeeCensusData(ABC):
     @staticmethod
     @abstractmethod
     def format_employee_data(employee_data_records: list[EmployeeDataRecord]) -> File:
