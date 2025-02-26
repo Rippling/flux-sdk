6a37d98dcfd3bf8510dda70b9db23ff3970dc1dd
Shanmukha Rao <shrao@rippling.com>
2025-02-26 20:58:01 +0530

    sort

--- flux_sdk/flux_core/data_models.py
+++ flux_sdk/flux_core/data_models.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 from decimal import Decimal
 from enum import Enum
-from typing import Optional, Any
+from typing import Any, Optional
 
 
 class LeaveType(Enum):
