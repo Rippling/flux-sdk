2adb316c01a00ecc731d9bdd582d684096100c6e
crohilla <crohilla@rippling.com>
2025-03-27 19:48:54 +0530

    Fixing the python comment

--- flux_sdk/payroll/report_employee_ytd_data/interface.py
+++ flux_sdk/payroll/report_employee_ytd_data/interface.py
@@ -7,10 +7,9 @@ class ReportEmployeeYTDData(ABC):
     @staticmethod
     @abstractmethod
     def format_employee_ytd_data(employee_ytd_data: ReportData) -> File:
-        """A function that takes a list of employee records and returns a formatted file.
+        """
+        Formats employee YTD data - ReportData into a file for third-party integration.
 
-        Given a list of EmployeeDataRecord, format into a file based on the
-        third party app's specs and upload it to a remote server.
-        :param employee_ytd_data: Employee YTD information in dictionary format where keys are object graph fields IDs.
-        :return: File
+        This function takes employee YTD records, structures them according to the
+        specifications of a third-party application, and generates a formatted file.
         """
