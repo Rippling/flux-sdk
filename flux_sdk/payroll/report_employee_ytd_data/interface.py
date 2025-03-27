59991d40a4ff56c1885f4ce4488d4e064df56ffb
Chirag Rohilla <crohilla@rippling.com>
2025-03-27 19:52:53 +0530

    GPDEV-36076 - Adding Payroll YTD Related Interfaces in Flux SDK (#128)
    * Adding payroll related interfaces in flux-sdk
    
    * Fixing the python comment

---
+++ flux_sdk/payroll/report_employee_ytd_data/interface.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+from flux_sdk.flux_core.data_models import File, ReportData
+
+
+class ReportEmployeeYTDData(ABC):
+    @staticmethod
+    @abstractmethod
+    def format_employee_ytd_data(employee_ytd_data: ReportData) -> File:
+        """
+        Formats employee YTD data - ReportData into a file for third-party integration.
+
+        This function takes employee YTD records, structures them according to the
+        specifications of a third-party application, and generates a formatted file.
+        """
